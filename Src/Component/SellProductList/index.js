29f2f6d3ef318b43f116537bf42607d6302f1fba
mdefteaz <efteaz@technoexponent.com>
2022-02-11 00:46:13 +0530

    icon crop issue fixed

--- Src/Component/SellProductList/index.js
+++ Src/Component/SellProductList/index.js
@@ -14,7 +14,7 @@ const SellProductList = (props) => {
         <TouchableOpacity onPress={() =>props.handelProduct(props.name)} style={{backgroundColor:'#ffffff',width:Devicewidth/3.5,height:Deviceheight/8,alignItems:'center',justifyContent:'center',}}>
         <View style={styles.Container}>
             <View style={{backgroundColor:"#fff",alignItems:'center',justifyContent:'center',height:Deviceheight/16,width:Devicewidth/8,borderRadius:360,marginTop:2}} onPress={getAdd}>
-            <Image source={{uri: props.imagePath+props.image}} style={{height:"100%",width:'100%',resizeMode:"contain",borderRadius:360}}/>
+            <Image source={{uri: props.imagePath+props.image}} style={{height:"100%",width:'100%',borderRadius:360}}/>
             </View>
             <Text style={{marginTop:5,color:"#000",fontSize:10,textAlign:'center',alignSelf:'center',fontWeight:'bold',width:Devicewidth/4}}>{props.name}</Text>
         </View>
